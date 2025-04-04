bde583288bb04028224df088f5727929c6eab5e4
holbok.peti <holbok.peter@students.jedlik.eu>
2025-04-04 13:24:41 +0200

    mynameIsJeff

---
+++ AllatokCsoportMunka/Allat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllatokCsoportMunka
+{
+    class Allat
+    {
+        public string Név { get; set; }
+        public string Kontinens {  get; set; }
+        public int Elettartam { get; set; }
+        public string Táplálék {  get; set; }
+        public string Osztály {  get; set; }
+
+        public Allat(string[] sorok)
+        {
+            foreach (var sor in sorok)
+            {
+                string[] adatok = sor.Split(",");
+                Név = adatok[0];
+                Kontinens = adatok[1];
+                Elettartam = int.Parse(adatok[2]);
+                Táplálék = adatok[3];
+                Osztály = adatok[4];
+            }
+        }
+    }
+}
---
+++ AllatokCsoportMunka/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AllatokCsoportMunka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
